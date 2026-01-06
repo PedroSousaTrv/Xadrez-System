f0c1f5e4419dccd87cf9cab33b95d615155b8b58
Pedro de Sousa <pedrosousa2003@gmail.com>
2026-01-06 19:32:28 -0300

    Implements Board-Piece

--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -5,4 +5,11 @@ public class Piece {
     protected Position position;
     private Board board;
 
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
 }
