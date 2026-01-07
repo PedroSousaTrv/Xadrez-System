64edb6bbe713085d939c96a2e23b292e44f5bfe8
Pedro de Sousa <pedrosousa2003@gmail.com>
2026-01-07 19:29:02 -0300

    Board Exeption

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,6 +27,6 @@ public class ChessMatch {
 
     private void initialSetup(){
         board.PlacePiece(new Rook(board,Color.WHITE),new Position(2,1));
-        board.PlacePiece(new King(board,Color.BLACK),new Position(6,7));
+        board.PlacePiece(new King(board,Color.BLACK),new Position(2,7));
     }
 }
